b924b8c0de7fbc0409370fa241277b909b38db80
Gowtham Krishna <153527821+Gauthamgauth@users.noreply.github.com>
2025-03-12 23:09:27 +0530

    Add files via upload

---
+++ lasso regression 2 .py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+from sklearn.datasets import load_diabetes
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import Lasso
+from sklearn.metrics import r2_score
+
+from sklearn.model_selection import train_test_split
+
+
+# In[19]:
+
+
+data = load_diabetes()
+
+df = pd.DataFrame(data.data,columns=data.feature_names)
+df["TARGET"] = data.target
+
+df.head()
+
+
+# In[25]:
+
+
+X_train,X_test,y_train,y_test = train_test_split(data.data,data.target,test_size=0.2,random_state=2)
+
+
+# In[27]:
+
+
+coefs = []
+r2_scores = []
+
+for i in[0,0.1,1,10]:
+    reg = Lasso(alpha=i)
+    reg.fit(X_train,y_train)
+    
+    coefs.append(reg.coef_.tolist())
+    y_pred = reg.predict(X_test)
+    r2_scores.append(r2_score(y_test,y_pred))
+
+
+# In[29]:
+
+
+plt.figure(figsize=(14,9))
+plt.subplot(221)
+plt.bar(data.feature_names,coefs[0])
+plt.title("Alpha = 0,r2_score={}".format(round(r2_scores[0],2)))
+
+plt.subplot(222)
+plt.bar(data.feature_names,coefs[1])
+plt.title("Alpha = 0.1,r2_score={}".format(round(r2_scores[1],2)))
+
+plt.subplot(223)
+plt.bar(data.feature_names,coefs[2])
+plt.title("Alpha = 1,r2_score={}".format(round(r2_scores[2],2)))
+
+plt.subplot(223)
+plt.bar(data.feature_names,coefs[3])
+plt.title("Alpha = 10,r2_score={}".format(round(r2_scores[3],2)))
+
